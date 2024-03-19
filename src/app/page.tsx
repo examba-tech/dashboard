9e41d66897969815267aa63dad065134ee31ef52
Marc Franquesa Monés <marc.franquesam@gmail.com>
2024-03-19 08:24:38 +0100

    Update main page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,52 +5,47 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div>
-      <div className="flex flex-col items-center justify-center pt-20">
-        <Image src="/examba.png" width={300} height={300} alt="examba" />
-        <div className="pt-4" />
-        <Link
-          href="/analytics"
-          className={buttonVariants({
-            className: "w-full max-w-xs",
-          })}
-        >
-          Entra a la página de análisis
-        </Link>
-      </div>
-      <div className="flex justify-center py-2">
-        <Link
-          href="/dashbord"
-          className={buttonVariants({
-            className: "w-full max-w-80 ",
-          })}
-        >
-          Entra a la página de dashboard
-        </Link>
+    <div className="flex flex-col items-center justify-center pt-20">
+      <Image src="/examba.png" width={300} height={300} alt="examba" />
+      <div className="pt-4" />
+      <Link
+        href="/analytics"
+        className={buttonVariants({
+          className: "w-full max-w-xs",
+        })}
+      >
+        Entra
+      </Link>
+      <div className="pt-4" />
+      <Link
+        href="/dashbord"
+        className={buttonVariants({
+          className: "w-full max-w-80 ",
+        })}
+      >
+        Entra a la página de dashboard
+      </Link>
 
-        <h2 className="text-xl font-semibold pt-40">Sobre Nosotros</h2>
-        <p className="mt-4 text-base max-w-xl text-justify">
-          EXAMBA és una nova iniciativa que hem tirat endavant deu estudiants de
-          la UPC especialitzats en Ciència i Enginyeria de Dades. Col·laborem
-          amb ciutadans, serveis mèdics i organismes públics, desenvolupant
-          algoritmes preventius que alerten sobre problemes de salut i
-          contaminació. Busquem conscienciar sobre el creixent perill de la
-          pol·lució en el nostre benestar i salut mitjançant la ciència de
-          dades.
-        </p>
+      <h2 className="text-xl font-semibold pt-20">Nosaltres</h2>
+      <p className="mt-4 text-base max-w-xl text-justify">
+        EXAMBA és una nova iniciativa que hem tirat endavant deu estudiants de
+        la UPC especialitzats en Ciència i Enginyeria de Dades. Col·laborem amb
+        ciutadans, serveis mèdics i organismes públics, desenvolupant algoritmes
+        preventius que alerten sobre problemes de salut i contaminació. Busquem
+        conscienciar sobre el creixent perill de la pol·lució en el nostre
+        benestar i salut mitjançant la ciència de dades.
+      </p>
 
-        <div className="pt-4" />
-        <Link
-          href="/about"
-          className={buttonVariants({
-            className: "w-full max-w-xs",
-            variant: "link",
-          })}
-        >
-          Leer más
-        </Link>
-      </div>
+      <div className="pt-4" />
+      <Link
+        href="/about"
+        className={buttonVariants({
+          className: "w-full max-w-xs",
+          variant: "link",
+        })}
+      >
+        Leer más
+      </Link>
     </div>
   );
 }
-
