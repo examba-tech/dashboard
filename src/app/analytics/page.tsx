365df40248c0e41d845d9e69c865c77632fd8fa3
Marc Franquesa Monés <marc.franquesam@gmail.com>
2024-04-20 19:34:33 +0200

    Add getcollections to utils

--- src/app/analytics/page.tsx
+++ src/app/analytics/page.tsx
@@ -1,23 +1,8 @@
 import Main from "@/src/components/table/main";
-
-const getMovies = async () => {
-  try {
-    const res = await fetch("http://localhost:3000/api/mongo/movies", {
-      cache: "no-store",
-    });
-
-    if (!res.ok) {
-      throw new Error("Failed to fetch topics");
-    }
-
-    return res.json();
-  } catch (error) {
-    console.log("Error loading topics: ", error);
-  }
-};
+import { getMongoCollection } from "@/src/utils/get_mongo_collection";
 
 const Page = async () => {
-  const data = await getMovies();
+  const data = await getMongoCollection("movies");
 
   const movies = data && data.collection ? data.collection : [];
 
--- src/app/prueba_union_graficos/page.tsx
+++ src/app/prueba_union_graficos/page.tsx
@@ -2,45 +2,14 @@ import React from "react";
 import MyLineChart from "@/src/components/charts/line_chart";
 import MyBarChart from "@/src/components/charts/bar_chart";
 import ReactVirtualizedTable from "@/src/components/table/tables/virtualTable";
-
-const getVisits = async () => {
-  try {
-    const res = await fetch("http://localhost:3000/api/mongo/visits", {
-      cache: "no-store",
-    });
-
-    if (!res.ok) {
-      throw new Error("Failed to fetch topics");
-    }
-
-    return res.json();
-  } catch (error) {
-    console.log("Error loading topics: ", error);
-  }
-};
-
-const getMovies = async () => {
-  try {
-    const res = await fetch("http://localhost:3000/api/mongo/movies", {
-      cache: "no-store",
-    });
-
-    if (!res.ok) {
-      throw new Error("Failed to fetch topics");
-    }
-
-    return res.json();
-  } catch (error) {
-    console.log("Error loading topics: ", error);
-  }
-};
+import { getMongoCollection } from "@/src/utils/get_mongo_collection";
 
 const Page = async () => {
-  const data = await getVisits();
+  const data = await getMongoCollection("visits");
 
   const visits = data && data.collection ? data.collection : [];
 
-  const dataa = await getMovies();
+  const dataa = await getMongoCollection("movies");
 
   const movies = dataa && dataa.collection ? dataa.collection : [];
 
