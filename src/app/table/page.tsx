41c91cced379fe307304d0f79d1bdfe43016f31a
Marc Franquesa Monés <marc.franquesam@gmail.com>
2024-04-21 11:39:30 +0200

    Add loading to table + caching

--- src/app/table/page.tsx
+++ src/app/table/page.tsx
@@ -1,14 +1,38 @@
+"use client";
+
+import * as React from "react";
 import MasterTable from "@/src/components/tables/master_table/main";
 import { getMongoCollection } from "@/src/utils/get_mongo_collection";
+import CircularProgress from "@mui/material/CircularProgress";
+import Box from "@mui/material/Box";
 
-const Page = async () => {
-  const data = await getMongoCollection("master");
+const Page = () => {
+  const [master, setMaster] = React.useState([]);
+  const [loading, setLoading] = React.useState(true);
 
-  const master = data && data.collection ? data.collection : [];
+  React.useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const data = await getMongoCollection("master");
+        setMaster(data && data.collection ? data.collection : []);
+        setLoading(false);
+      } catch (error) {
+        console.error("Error fetching data:", error);
+        setLoading(false);
+      }
+    };
+
+    fetchData();
+  }, []);
 
   return (
     <>
-      <MasterTable data={master} />
+      {loading && (
+        <Box className="flex justify-center items-center h-96">
+          <CircularProgress />
+        </Box>
+      )}
+      {!loading && <MasterTable data={master} />}
     </>
   );
 };
--- src/utils/get_mongo_collection.ts
+++ src/utils/get_mongo_collection.ts
@@ -1,5 +1,9 @@
 export async function getMongoCollection(collection_name: string) {
   try {
+    const cachedData = localStorage.getItem(collection_name);
+    if (cachedData) {
+      return JSON.parse(cachedData);
+    }
     const res = await fetch(
       "http://localhost:3000/api/mongo/" + collection_name,
       {
@@ -11,7 +15,18 @@ export async function getMongoCollection(collection_name: string) {
       throw new Error("Failed to fetch topics");
     }
 
-    return res.json();
+    const jsonData = await res.json();
+
+    await new Promise<void>((resolve, reject) => {
+      try {
+        localStorage.setItem(collection_name, JSON.stringify(jsonData));
+        resolve();
+      } catch (error) {
+        reject(error);
+      }
+    });
+
+    return jsonData;
   } catch (error) {
     console.log("Error loading topics: ", error);
   }
