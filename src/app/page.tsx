5314027cda8fc8dc792ed02d7eb9a0182794c1d2
mariarisques <106369869+mariarisques@users.noreply.github.com>
2024-04-29 22:27:28 +0200

    login evoluciona favorablement (#53)
    * login evoluciona favorablement
    
    * P*** build
    
    * retoque
    
    * '
    
    ---------
    
    Co-authored-by: Maria Risques <mariarisques@gmail.com>

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,7 @@ import { buttonVariants } from "@/src/components/ui/Button";
 import Link from "next/link";
 import Image from "next/image";
 import type { Metadata } from "next";
+import SignIn from "../components/signin/singin";
 
 export const metadata: Metadata = {
   title: "Examba",
@@ -10,19 +11,13 @@ export const metadata: Metadata = {
 
 export default function Home() {
   return (
-    <div className="flex flex-col items-center justify-center">
-      <Image src="/examba.png" width={500} height={500} alt="examba" />
-      <div className="pt-4" />
-      <Link
-        href="/auth/signin"
-        className={buttonVariants({
-          className: "w-full max-w-xs",
-          variant: "ghost",
-        })}
-      >
-        Sign in
-      </Link>
-      <h2 className="text-xl font-semibold pt-20">Nosaltres</h2>
+    <div className="flex flex-col items-center justify-center pt-20">
+      <Image src="/examba.png" width={600} height={600} alt="examba" />
+      <div className="w-full max-w-2xl"> 
+      <SignIn />
+      <div className="flex flex-col items-center justify-center pt-10">
+      <h2 className="text-xl font-semibold pt-4">Nosaltres</h2>
+
       <p className="mt-4 text-base max-w-xl text-justify">
         EXAMBA és una nova iniciativa que hem tirat endavant deu estudiants de
         la UPC especialitzats en Ciència i Enginyeria de Dades. Col·laborem amb
@@ -31,8 +26,6 @@ export default function Home() {
         conscienciar sobre el creixent perill de la pol·lució en el nostre
         benestar i salut mitjançant la ciència de dades.
       </p>
-
-      <div className="pt-4" />
       <Link
         href="/about"
         className={buttonVariants({
@@ -42,6 +35,8 @@ export default function Home() {
       >
         Més...
       </Link>
+      </div>
+      </div>
     </div>
   );
 }
