40c91d710a0f14afa8f93d2d3e02ac59691be0fc
Marc Franquesa Monés <marc.franquesam@gmail.com>
2024-04-29 16:34:15 +0200

    hotfix: remove local storage

--- src/utils/get_mongo_collection.ts
+++ src/utils/get_mongo_collection.ts
@@ -1,27 +1,17 @@
 export async function getMongoCollection(collection_name: string) {
   try {
-    if (typeof window !== "undefined") {
-      const cachedData = localStorage.getItem(collection_name);
-      if (cachedData) {
-        return JSON.parse(cachedData);
-      }
-    }
-
     const res = await fetch(
       "http://localhost:3000/api/mongo/" + collection_name,
       {
         cache: "force-cache",
       }
     );
-    
+
     if (!res.ok) {
       throw new Error("Failed to fetch topics");
     }
 
     const jsonData = await res.json();
-    if (typeof window !== "undefined") {
-      localStorage.setItem(collection_name, JSON.stringify(jsonData));
-    }
 
     return jsonData;
   } catch (error) {
