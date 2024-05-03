1aa6f97f2c44d066a542fe81c5be782f9bff8d30
Marc Franquesa <110655741+marcfranquesa@users.noreply.github.com>
2024-05-03 14:30:25 +0200

    Fix double footer (#71)

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,15 +1,22 @@
 "use client";
+import Sidebar from "@/src/components/ui/Sidebar";
 import Footer from "@/src/components/ui/Footer";
 import { cn } from "@/src/utils/utils";
 import "@/src/styles/globals.css";
 import React from "react";
 import { montserrat } from "@/src/components/ui/fonts";
+import { usePathname } from "next/navigation";
 
 export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const [isVisible, setIsVisible] = React.useState(false);
+  const pathname = usePathname();
+
+  const inLogin = pathname === "/";
+
   return (
     <html
       lang="en"
@@ -18,7 +25,14 @@ export default function RootLayout({
         montserrat.className
       )}
     >
-      <body>
+      <body
+        className={`min-h-screen pt-12 bg-slate-50 antialiased ${
+          !inLogin ? (isVisible ? "ml-64" : "ml-16") : ""
+        } transition-width duration-300`}
+      >
+        {!inLogin && (
+          <Sidebar isVisible={isVisible} setIsVisible={setIsVisible} />
+        )}
         <div className="container max-w-7xl mx-auto h-full pt-10 min-h-screen">
           {children}
         </div>
