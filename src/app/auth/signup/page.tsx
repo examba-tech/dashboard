91ef1285fd2ae39c89046bdc958fa567dfa3a936
Maria Risques <mariarisques@gmail.com>
2024-05-10 18:21:53 +0200

    Torna a l'inci Nosaltres + SignUp

--- src/app/auth/signup/page.tsx
+++ src/app/auth/signup/page.tsx
@@ -1,14 +1,39 @@
+"use client"
 import React from "react";
 import Link from "next/link";
 import { buttonVariants } from "@/src/components/ui/Button";
 
 const SignUp: React.FC = () => {
+  const handleLogout = () => {
+    window.location.href = "/";
+  };
   return (
     <div className="justify-center">
       <div className="rounded-sm border border-stroke bg-white shadow-default p-4 sm:p-12.5 max-w-lg mx-auto dark:border-strokedark dark:bg-boxdark">
         <h2 className="mb-9 text-2xl font-bold text-black dark:text-white sm:text-title-xl2">
           Crea un compte a EXAMBA
         </h2>
+        <Link 
+          onClick={handleLogout}
+          style={{
+            position: 'fixed', // Posición fija en la pantalla
+            top: '20px', // 20 píxeles desde la parte superior
+            left: '20px', // 20 píxeles desde la parte izquierda
+            padding: '10px 20px', // Espaciado interno del botón
+            fontSize: '16px', // Tamaño de fuente del texto
+            border: 'none', // Sin borde
+            borderRadius: '5px', // Bordes redondeados
+            cursor: 'pointer', // Cursor en forma de puntero
+            textDecoration: 'none' // Elimina la decoración de texto por defecto
+          }}
+          href="/" 
+          className={buttonVariants({
+            className: "w-full max-w-xs",
+            variant: "ghost",
+          })}
+        >
+          Torna a l&apos;Inici
+      </Link>
         <form>
 
           <label className="block font-medium text-black dark:text-white mb-2.5">
