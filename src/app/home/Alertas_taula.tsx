01583e45c2715f4e9534dee4f21ded0ce787e43d
BlancaUG <106520667+BlancaUG@users.noreply.github.com>
2024-05-17 08:58:12 +0200

    home page añadiendo tb el acceso desde el signin y en la sidebar + ca… (#145)
    * home page añadiendo tb el acceso desde el signin y en la sidebar + cambio de algunos de los iconos de la sidebar
    
    * home page y cambios del side bar
    
    * home page y side bar
    
    * home page + sidebar + iconos
    
    ---------
    
    Co-authored-by: BlancaUG <blancaunanuegambra@gmail.com>
    Co-authored-by: mariarisques <106369869+mariarisques@users.noreply.github.com>

--- src/app/home/Alertas_taula.tsx
+++ src/app/home/Alertas_taula.tsx
@@ -23,7 +23,7 @@ const PredictionsAlerts: React.FC<PredictionsAlertsProps> = ({ predictions }) =>
     if (Number(pred.INGRESSOS_AVG) > Number(1.9)) {
       alerts.push(
         <Alert severity="error" style={{ marginBottom: '10px' }}>
-          Compte! El municipi {pred.Nom_municipi} té una previsió d&apos;alts ingresos para la pròxima setmana.
+          Compte! El municipi <strong>{pred.Nom_municipi}</strong> té una previsió d&apos;<strong>alts</strong> ingresos per la propera setmana.
         </Alert>
       );
     }
--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -4,6 +4,8 @@ import Alertas_taula from "./Alertas_taula";
 import { getMongoCollection } from "@/src/utils/get_mongo_collection";
 import CircularProgress from "@mui/material/CircularProgress";
 import Box from "@mui/material/Box";
+import Link from "next/link";
+import Image from "next/image";
 
 const Page = () => {
   const [preds, setPreds] = React.useState<
@@ -44,13 +46,93 @@ const Page = () => {
 
   return (
     <div>
-      {loading && (
-        <Box className="flex justify-center items-center h-96">
-          <CircularProgress />
-        </Box>
-      )}
-      {!loading && <Alertas_taula predictions={preds} />}
+      <div>
+        <h1 style={{ fontSize: "2rem", fontWeight: "bold" }}>
+            <div className="overflow-y-auto" style={{ maxHeight: '200px', overflowY: 'auto' }}>HOLA DE NOU! 👋🏼</div>
+        </h1> 
+      </div>
+      <div className="flex justify-center items-center mt-10 space-x-8">
+        <Link href={"/dashboard/malalties_dinamiques"}>
+          <div className="option-box flex flex-col justify-center items-center rounded-lg border border-primary bg-blue-100 p-4 text-black transition hover:bg-opacity-90 mr-4 cursor-pointer" style={{ height: "200px", width: "200px" }}>
+            <div className="mb-1">
+              <Image
+                    src="/bar-chart-regular-24-negro.png"
+                    alt="Icon3"
+                    width={24}
+                    height={24}
+                    className="w-8 h-8 ml-5 mr-2 opacity-1"
+              />
+            </div>
+            <p className="text-center text-2xl font-bold">{"Patologies Agudes"}</p>
+          </div>
+        </Link>
+        <Link href={"/dashboard/malalties_estatiques"}>
+          <div className="option-box flex flex-col justify-center items-center rounded-lg border border-primary bg-blue-100 p-4 text-black transition hover:bg-opacity-90 mr-4 cursor-pointer" style={{ height: "200px", width: "200px"}}>
+            <div className="mb-1">
+                <Image
+                      src="/pie-chart-alt-2-regular-24 (1).png"
+                      alt="Icon3"
+                      width={24}
+                      height={24}
+                      className="w-8 h-8 ml-5 mr-2 opacity-1"
+                />
+              </div>
+            <p className="text-center text-2xl font-bold">{"Malalties de prevalença"}</p>
+          </div>
+        </Link>
+        <Link href={"/dashboard/table"}>
+          <div className="option-box flex flex-col justify-center items-center rounded-lg border border-primary bg-blue-100 p-4 text-black transition hover:bg-opacity-90 mr-4 cursor-pointer" style={{ height: "200px", width: "200px" }}>
+            <div className="mb-1">
+                <Image
+                      src="/table-regular-24-negro.png"
+                      alt="Icon3"
+                      width={24}
+                      height={24}
+                      className="w-8 h-8 ml-5 mr-2 opacity-1"
+                />
+              </div>
+            <p className="text-center text-2xl font-bold">{"Descàrrega de dades"}</p>
+          </div>
+        </Link>
+        <Link href={"/dashboard/estacions_meteo"}>
+          <div className="option-box flex flex-col justify-center items-center rounded-lg border border-primary bg-blue-100 p-4 text-black transition hover:bg-opacity-90 mr-4 cursor-pointer" style={{ height: "200px", width: "200px" }}>
+            <div className="mb-1">
+                <Image
+                      src="/cloud-lightning-regular-24 (1)-negro.png"
+                      alt="Icon3"
+                      width={24}
+                      height={24}
+                      className="w-8 h-8 ml-5 mr-2 opacity-1"
+                />
+              </div>
+            <p className="text-center text-2xl font-bold">{"Estacions meteorològiques"}</p>
+          </div>
+        </Link>
+        <Link href={"/dashboard/estacions_contaminacio"}>
+          <div className="option-box flex flex-col justify-center items-center rounded-lg border border-primary bg-blue-100 p-4 text-black transition hover:bg-opacity-90 mr-4 cursor-pointer" style={{ height: "200px", width: "200px" }}>
+            <div className="mb-1">
+                <Image
+                      src="/world-regular-24 (1).png"
+                      alt="Icon3"
+                      width={24}
+                      height={24}
+                      className="w-8 h-8 ml-5 mr-2 opacity-1"
+                />
+              </div>
+            <p className="text-center text-2xl font-bold">{"Estacions de contaminació"}</p>
+          </div>
+        </Link>
+      </div>
+      <div className="mt-10">
+        {loading && (
+          <Box className="flex justify-center items-center h-96">
+            <CircularProgress />
+          </Box>
+        )}
+        {!loading && <Alertas_taula predictions={preds} />}
+      </div>
     </div>
+    
   );
 };
 
